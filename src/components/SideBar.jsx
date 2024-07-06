f47ff03435ab66ec13b46921c6db899fd4eb192f
Johnny Lin <johnlin2922@gmail.com>
2024-07-06 14:55:49 -0500

    add unfinished projects dropdown for the lolz

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -8,6 +8,7 @@ const ReactIcon = require("../assets/icons/ReactIcon.png");
 const SideBar = () => {
     const [showProjects, setShowProjects] = useState(true);
     const [showWebList, SetShowWebList] = useState(true);
+    const [showInProgressList, SetShowInProgressList] = useState(true);
 
     return (
         <div className="flex h-full">
@@ -22,7 +23,7 @@ const SideBar = () => {
                         ) : (
                             <ChevronRightIcon className="w-7 mr-1" />
                         )}
-                        <p>SideBarStuff</p>
+                        <p>Projects</p>
                     </div>
                     {showProjects ? (
                         <>
@@ -39,6 +40,26 @@ const SideBar = () => {
                                 <p>Web</p>
                             </div>
                             {showWebList ? <WebList /> : null}
+                        </>
+                    ) : null}
+
+                    {showProjects ? (
+                        <>
+                            <div
+                                className="flex items-center hover:cursor-pointer hover:bg-opacity-80 hover:bg-[#2b2a2a] font-bold"
+                                onClick={() =>
+                                    SetShowInProgressList(!showInProgressList)
+                                }
+                            >
+                                {showInProgressList ? (
+                                    <ChevronDownIcon className="w-7 mr-1  ml-5" />
+                                ) : (
+                                    <ChevronRightIcon className="w-7 mr-1  ml-5" />
+                                )}
+
+                                <p>In Progress</p>
+                            </div>
+                            {showInProgressList ? <InProgressList /> : null}
                         </>
                     ) : null}
                 </div>
@@ -77,6 +98,51 @@ const WebList = () => (
                     className="w-7 mr-1  ml-5 text-yellow_vs"
                 />
                 <p>Restaurant Web App</p>
+            </div>
+        </a>
+        <a href="/">
+            <div className="flex items-center hover:cursor-pointer hover:bg-opacity-80 hover:bg-[#2b2a2a] ml-12">
+                <img
+                    src={JSIcon}
+                    alt="JS Icon"
+                    className="w-7 mr-1  ml-5 text-yellow_vs"
+                />
+                <p>Fourth Project</p>
+            </div>
+        </a>
+    </div>
+);
+
+const InProgressList = () => (
+    <div className="flex flex-col">
+        <a href="/">
+            <div className="ml-12 flex items-center hover:cursor-pointer hover:bg-opacity-80 hover:bg-[#2b2a2a]">
+                <img
+                    src={JSIcon}
+                    alt="JS Icon"
+                    className="w-7 mr-1  ml-5 text-yellow_vs"
+                />
+                <p>Weather Hood</p>
+            </div>
+        </a>
+        <a href="/">
+            <div className="ml-12 flex items-center hover:cursor-pointer hover:bg-opacity-80 hover:bg-[#2b2a2a]">
+                <img
+                    src={ReactIcon}
+                    alt="TS Icon"
+                    className="w-7 mr-1  ml-5 text-yellow_vs"
+                />
+                <p>Stonks</p>
+            </div>
+        </a>
+        <a href="/">
+            <div className="flex items-center hover:cursor-pointer hover:bg-opacity-80 hover:bg-[#2b2a2a] focus:bg-slate-300 ml-12">
+                <img
+                    src={JSIcon}
+                    alt="JS Icon"
+                    className="w-7 mr-1  ml-5 text-yellow_vs"
+                />
+                <p>Amazing Countries</p>
             </div>
         </a>
         <a href="/">

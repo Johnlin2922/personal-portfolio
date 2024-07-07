40fc296f4cdddb6f263dc54444d26dedb6d00258
Johnny Lin <johnlin2922@gmail.com>
2024-07-07 14:23:23 -0500

    add utilities class, utilize utility class to dynamically render padding based on screen size

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,20 +5,34 @@ import Contact from "./Contact";
 import Header from "../components/Header";
 import About from "./About";
 import Resume from "./Resume";
+import { useWindowSize } from "../Utilities/Utilities";
 
 const Home = () => {
     const [activeTab, setActiveTab] = useState("home");
     const sideBarWidth = 270;
 
+    const windowSize = useWindowSize();
+
+    const getPaddingLeftClass = () => {
+        if (windowSize.width >= 1024) {
+            return `pl-[${sideBarWidth}px]`;
+        } else {
+            return "pl-[0px]";
+        }
+    };
+
     return (
         <div className="App flex h-full">
             <div
-                className="bg-[#262526] h-full fixed hidden lg:block"
+                className="bg-[#262526] h-full fixed hidden lg:block sm:pl-0"
                 style={{ width: `${sideBarWidth}px` }}
             >
                 <SideBar />
             </div>
             <div className="bg-[#1e1e1e] h-full">
+                <div className="lg:hidden">
+                    <div>insert mobile menu here</div>
+                </div>
                 <div
                     className="bg-[#424042] h-16 hidden lg:block"
                     style={{ paddingLeft: `${sideBarWidth}px` }}
@@ -26,10 +40,7 @@ const Home = () => {
                     <NavBar activeTab={activeTab} setActiveTab={setActiveTab} />
                 </div>
                 {activeTab === "home" && (
-                    <div
-                        style={{ paddingLeft: `${sideBarWidth}px` }}
-                        className="w-screen"
-                    >
+                    <div className={`w-screen ${getPaddingLeftClass()}`}>
                         <Header />
                         <About />
                         <Resume />

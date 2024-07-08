baa3197216bb3c913e2b5672507d7d9ed146241f
Johnny Lin <johnlin2922@gmail.com>
2024-07-07 19:28:04 -0500

    update resumt

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -61,17 +61,75 @@ const Resume = () => {
                         </code>
                         <br />
                         <code className="text-m">
-                            <br />• Lorem ipsum dolor sit amet, consectetur
-                            adipiscing elit.
-                            <br />• Sed id eros non nisi vulputate eleifend.
-                            Vestibulum erat ipsum, lacinia ut nibh ut, pulvinar
-                            interdum lorem.
-                            <br />• Aliquam et nisi eu risus ultrices suscipit
-                            nec nec quam.
-                            <br />• Suspendisse pretium fermentum luctus.
-                            <br />• Maecenas vitae vestibulum ipsum, ut varius
-                            diam.
-                            <br />• Phasellus mattis nunc vel purus maximus.
+                            <br />• Software Engineer for Unmanned Aerial
+                            Systems Arbitration Suite which provides command and
+                            control functionality to several air platforms
+                            through our API.
+                            <br />• Develop custom Web Clients, API testing
+                            tools, along with Android Tactical Assault Kit
+                            plugins to interface with Arbitration Suite's API.
+                            <br />• Android Development in support of DARPA's
+                            Mobile Force Protection project.
+                        </code>
+                        <br />
+                        <br />
+                        <code className="text-blue_vs text-2xl">
+                            Yamato Steakhouse
+                        </code>
+                        <br />
+                        <code className="italic text-m text-lightblue_vs">
+                            Jacksonville, AL{" "}
+                        </code>
+                        <br />
+                        <code className="text-s text-brown_vs">
+                            • January 2019 - Present
+                        </code>
+                        <br />
+                        <code className="text-m">
+                            <br />• I developed and maintained an online
+                            ordering system for a local restaurant. This
+                            involved designing the user interface, ensuring the
+                            functionality of the website, and integrating
+                            third-party APIs to facilitate phone calls, text
+                            messages, and email notifications. I also repurposed
+                            an existing iPad for email order notifications.
+                            <br />• During the pandemic, I played an
+                            instrumental role in helping the restaurant continue
+                            their business operations by delivering a reliable
+                            and efficient online ordering platform. By
+                            collaborating effectively with the management team,
+                            I was able to expand the restaurant's customer base
+                            and increase revenue, despite the challenging
+                            circumstances.
+                        </code>
+
+                        <br />
+                        <br />
+                        <code className="text-blue_vs text-2xl">
+                            Graduate Teaching Assistant
+                        </code>
+                        <br />
+                        <code className="italic text-m text-lightblue_vs">
+                            Jacksonville, AL{" "}
+                        </code>
+                        <br />
+                        <code className="text-s text-brown_vs">
+                            • January 2021 - May 2021
+                        </code>
+                        <br />
+                        <code className="text-m">
+                            <br />• During my last semester of graduate school.
+                            I worked as a Graduate Teaching Assistant at
+                            Jacksonville State University. In this capacity, I
+                            provided valuable support to Computer Science and
+                            Mathematics faculty members by facilitating student
+                            learning through individual and group tutoring
+                            sessions, as well as assessing their academic
+                            progress by grading papers. This role not only
+                            allowed me to hone my teaching and mentoring skills
+                            but also enabled me to make meaningful contributions
+                            towards the academic development of the university's
+                            students.
                         </code>
                     </div>
                 </div>
@@ -81,19 +139,16 @@ const Resume = () => {
                     </div>
                     <div className="md:w-3/4">
                         <p className="text-m font-mono">
-                            <br />• Nulla tristique luctus lacinia. Nullam ut
-                            tortor arcu.
-                            <br />• Aenean in mi non leo placerat suscipit a
-                            eget odio.
-                            <br />• Vivamus et dolor odio.
-                            <br />• Suspendisse tempus interdum eros, et
-                            bibendum massa tempus vitae.
-                            <br />• Sed in aliquam ligula. Sed eget orci tortor.
-                            <br />• Pellentesque laoreet laoreet justo, sed
-                            efficitur sapien tincidunt eu.
-                            <br />• Nulla tempor nunc eu molestie volutpat. Nunc
-                            a accumsan dolor.
-                            <br />• Duis ut congue dui.
+                            <br />• I speak Latin :)
+                            <br />• Lorem ipsum dolor sit amet, consectetur
+                            adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim
+                            veniam, quis nostrud exercitation ullamco laboris
+                            nisi ut aliquip ex ea commodo consequat. Duis aute
+                            irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur
+                            sint occaecat cupidatat non proident, sunt in culpa
+                            qui officia deserunt mollit anim id est laborum.
                         </p>
                     </div>
                 </div>
